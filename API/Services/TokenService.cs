5d08b6443c7ed4c5ef28e4f24232054d54cef94f
Abdurrahman <atherwi@gmail.com>
2021-07-24 20:07:32 +0300

    add files after modul 16

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.Activity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration _config;
+        public TokenService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string CreaateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescripter = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(2),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescripter);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+    }
+}
---
+++ Domain/Activity/AppUser.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Activity
+{
+    public class AppUser: IdentityUser
+    {
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+        
+        
+        public ICollection<ActivityAttendee> Activities { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        
+        
+        
+    }
+}
